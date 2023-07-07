78b9cd02e615c92e09e7702d3a7ee6a12296215f
cc j <ccj@192.168.5.109>
2023-07-07 23:14:23 +0800

    Initial commit

---
+++ src/test/java/com/jfc/TulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TulingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
